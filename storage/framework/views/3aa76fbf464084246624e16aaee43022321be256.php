cfc95b0f0172e584e2b6de6f0f59d45742b64d5b
aman kumar <62890537+ddshotama@users.noreply.github.com>
2022-04-02 12:50:30 +0530

    Add files via upload

---
+++ storage/framework/views/3aa76fbf464084246624e16aaee43022321be256.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Laravel 8 CRUD Application - HDTuto.com</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  
+
+<div class="container">
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+</div>
+
+   
+
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\blog\resources\views/products/layout.blade.php ENDPATH**/ ?>
